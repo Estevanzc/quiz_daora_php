19aa1602b0c6c86a51e46a3068dce691a06f52f3
Estevanzc <134285029+Estevanzc@users.noreply.github.com>
2023-12-04 10:56:24 -0300

    quiz_questions (ainda não)

---
+++ rank_arm.php
@@ -0,0 +1,15 @@
+<?php
+$user_score = $_GET["user_score"] ?? false;
+$action = $_GET["action"] ?? 1;
+if ($action == 0) {
+    session_start();
+    //$user_name = $_SESSION["user_name"] ?? false;
+    $user_name = $user_score;
+    $data = [];
+    if (file_exists("data.txt")) {
+        $data = json_decode(file_get_contents("data.txt"), true);
+    }
+    $data[] = [$user_name, floatval($user_score)];
+    file_put_contents("data.txt", json_encode($data));
+}
+?>
