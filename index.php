bd94e39d23dcdc6f582d10def5c025919f914c67
juliagpereira <134286296+juliagpereira@users.noreply.github.com>
2023-12-05 10:32:53 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <title>Seu Login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form action="inforlogin.php" method="get">
+        <h1> DBOA QUIZ</h1>
+        <div class="input-local">
+            <input type="text" placeholder="digite seu player" name="player">
+           <!--<i>crie um nome</i>-->
+        </div>
+        
+        <button type="submit" class="btn">Começar a jogar</button>
+
+    </form>
+    
+</body>
+</html>
